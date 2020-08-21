6c8763efeb11e0af35915262ee21f04460cc4ffa
Ronald Pitts <ronpitts81@gmail.com>
2020-08-20 20:56:38 -0400

    new update

--- app.js
+++ app.js
@@ -1,114 +1,11 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const outputPath = path.resolve(__dirname, "output", "team.html");
 const render = require("./lib/htmlRenderer");
-
-const teamMembers = [];
-const idArray = [];
-
-function appMenu() {
-    function createManager(){
-        console.log("Please enter your manager");
-        inquirer.prompt([ 
-            {
-                type: "input",
-                name: "managerName",
-                message: "What is your manager's name?"                                
-            },{
-                type: "input",
-                name: "managerId",
-                message: "What is your manager's id?"
-            },{
-                type: "input",
-                name: "managerEmail",
-                message: "What is your manager's email?"
-            },{
-                type: "input",
-                name: "managerOfficeNumber",
-                message: "What is your manager's office number?"
-            }
-        ]).then(answers => {
-            const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber)
-            teamMembers.push(manager);
-            idArray.push(answers.managerId);
-            createTeam();
-        })
-    }
-
-    function createTeam() {
-        inquirer.prompt([
-            {
-              type: "list",
-              name: "memberChoice",
-              message: "Which type of team member would you like to add?",
-              choices: [
-                "Engineer",
-                "Intern",
-                "I don't want to add any more team members"
-              ]
-            }
-            ]).then(userChoice => {
-            switch(userChoice.memberChoice) {
-            case "Engineer":
-              addEngineer();
-              break;
-            case "Intern":
-              addIntern();
-              break;
-            default:
-              buildTeam();
-            }
-        });
-    }
-
-    function addEngineer() {
-      inquirer.prompt([
-      {  
-          type: "input",
-          name: "engineerName",
-          message: "What is your engineer's name?"                                
-      },{
-        type: "input",
-          name: "engineerId",
-          message: "What is your engineer's ID?"                                
-      },{
-        type: "input",
-          name: "engineerEmail",
-          message: "What is your engineer's Email?"                                
-      },{
-        type: "input",
-          name: "engineerGithub",
-          message: "What is your engineer's Github?"                                
-      },{
-      }
-    ]);
-
-    }
-
-    function addIntern() {
-      inquirer.prompt([
-        {  
-            type: "input",
-            name: "internName",
-            message: "What is your intern's name?"                                
-        },{
-          type: "input",
-            name: "internId",
-            message: "What is your intern's ID?"                                
-        },{
-          type: "input",
-            name: "internEmail",
-            message: "What is your intern's Email?"                                
-        },{
-          type: "input",
-            name: "internSchool",
-            message: "What is your intern's School?"                                
-        },{
 let role;
 let name;
 let id;
@@ -125,7 +22,6 @@ const pickOne = () => {
             message: 'What is the persons role in this comoany?',
             choices: ['Engineer', 'Intern', 'Manager', 'No more employees']
         }
-      ]);
     ]).then(workers => {
         console.log(workers);
         role = workers['Employee role'];
@@ -177,30 +73,27 @@ const pickTwo = () => {
                 console.log(school);
                 pickThree();
             })
-            break;    
+            break;
         case 'No more employees':
             render(employees)
     }
-
-    function buildTeam() {
-
 }
 const pickThree = () => {
     inquirer.prompt([
         {
             type: 'input',
             name: 'Employee Name',
-            message: 'Enter employee name'     
+            message: 'Enter employee name'
         },
         {
             type: 'input',
             name: 'Employee ID',
-            message: 'Enter employee ID'     
+            message: 'Enter employee ID'
         },
         {
             type: 'email',
             name: 'Employee Email',
-            message: 'Enter employee email address'     
+            message: 'Enter employee email address'
         },
     ]).then(answers => {
         name = answers['Employee Name'];
@@ -225,15 +118,8 @@ const pickFour = () => {
         case 'Manager':
             employees.push(new Manager(name, id, email, school));
             pickOne();
-            break;    
+            break;
     }
 }
 pickOne();
 module.exports = employees;
-
-
-    createManager();
-
-}
-
-appMenu();
